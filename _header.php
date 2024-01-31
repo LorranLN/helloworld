4a3dd9e651b27368da41b7a9e30b795ab5999f35
LorranLN <lorrancarvalho84@gmail.com>
2024-01-30 21:39:44 -0300

    Update 0.5

---
+++ _header.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Insere o link das folhas de estilo do tema-->
+    <link rel="stylesheet" href="assets/css/global.css">
+    <title>Hello Word <?php echo $title ?></title>
+</head>
+
+<body>
+
+    <div id="wrap">
+
+        <header>
+
+            <div class="header-logo-title">
+                <a href="index.php" title="Página Inicial">
+                    <img src="assets/img/logo01 - Copia.png" alt="Logotipo de Hello World">
+                </a>
+                <!-- Titulo Exemplo 1-->
+                <div class="header-title">
+                    <h1>Hello Word</h1>
+                    <small>Lendo e aprendendo.</small>
+                </div>
+            </div>
+            <!-- Titulo Exemplo 2
+            <h1>
+                Hello World
+                <small>Lendo e aprendendo.</small>
+            </h1>
+            -->
+
+            <!-- Formulário de Buscas -->
+            <div class="header-search">
+
+                <form action="" method="get">
+
+                    <input type="search" name="q" id="search" placeholder="Procurar...">
+                    <button type="submit"><i class="fa-solid fa-magnifying-glass fa-fw "></i></button>
+                </form>
+
+            </div>
+
+        </header>
+
+        <nav>
+            <a href="index.php">
+                <i class="fa-solid fa-house fa-fw"></i>
+                <span>Início</span>
+            </a>
+
+            <a href="contacts.php">
+                <i class="fa-solid fa-comment fa-fw"></i>
+                <span>Contatos</span>
+            </a>
+
+            <a href="about.php">
+                <i class="fa-solid fa-circle-info"></i>
+                <span>Sobre</span>
+            </a>
+
+        </nav>
+
+        <main>
---
+++ contacts.php
@@ -0,0 +1,22 @@
+<?php
+$title = "- Meus Contatos";
+
+    require("_header.php");
+
+?>
+
+
+
+<article>
+    <h2> Meus Contatos</h2>
+ Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda accusamus magni ex aperiam fugit libero dolore explicabo corporis dolorum, nihil et. Nam officiis repudiandae porro voluptatibus velit quos ipsa dolor.
+    </article>
+
+<aside>
+    <h3>Contatos adicionais</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet molestias mollitia aliquam quasi eligendi rerum expedita ducimus assumenda nisi? Error expedita illo incidunt aut dicta, officiis enim quis obcaecati vel.</p>
+</aside>
+
+
+
+<?php require("_footer.php")?>
