1b207a73a1bec07c62ffe0b628d6670163b0cad7
LorranLN <lorrancarvalho84@gmail.com>
2024-03-26 21:40:59 -0300

    Update 15.0

--- search.php
+++ search.php
@@ -6,11 +6,10 @@ require("_global.php");
 // Configurações desta página
 $page = array(
     "title" => "Procurando...",
-    "css" => "index.css",
-    "js" => "search.js"
+    "css" => "index.css"
 );
 
-// Incializa variável da view
+// inicializa a view
 $search_view = '';
 
 // Incializa vatiável com total de comentários
@@ -22,25 +21,31 @@ $query = isset($_GET['q']) ? trim(htmlentities(strip_tags($_GET['q']))) : '';
 // Se a query NÃO está vazia
 if ($query != '') :
 
+    // Incializa variável da view
+    $search_view .= "<h2>Procurando por '{$query}'</h2>";
+
+    // Altera <title>
+    $page['title'] = "Procurando por '{$query}'";
+
     // Consulta SQL usa prepared statement
     $sql = <<<SQL
 
--- Referências: https://www.w3schools.com/mysql/mysql_like.asp
+    -- Referências: https://www.w3schools.com/mysql/mysql_like.asp
 
-SELECT 
-	art_id, art_thumbnail, art_title, art_summary 
-FROM article 
-WHERE
-	-- Requisitos padrão
-    art_date <= NOW()
-    AND art_status = 'on'
-    -- Busca
-    AND art_title LIKE ?
-    OR art_summary LIKE ?
-    OR art_content LIKE ?
-ORDER BY art_date DESC;
+        SELECT 
+            art_id 
+        FROM article 
+        WHERE
+            -- Requisitos padrão
+            art_date <= NOW()
+            AND art_status = 'on'
+            -- Busca
+            AND art_title LIKE ?
+            OR art_summary LIKE ?
+            OR art_content LIKE ?
+        ORDER BY art_date DESC;
 
-SQL;
+    SQL;
 
     // Prepara a query de busca
     $search_query = "%{$query}%";
@@ -54,6 +59,7 @@ SQL;
         $search_query
     );
     $stmt->execute();
+
     // Obtém o resultado da consulta
     $res = $stmt->get_result();
 
@@ -67,38 +73,15 @@ SQL;
         if ($total == 1) $viewtotal = '1 resultato';
         else $viewtotal = "{$total} resultados";
 
-        $search_view .= <<<HTML
-        
-<h2>Procurando por {$query}</h2>
-<p><small class="authordate">{$viewtotal}</small></p>
+        $search_view .= "<p><small class=\"authordate\">{$viewtotal}</small></p>";
 
-HTML;
-
-        while ($art = $res->fetch_assoc()) :
-
-            $search_view .= <<<HTML
-
-<div class="article" onclick="location.href = 'view.php?id={$art['art_id']}'">
-    <img src="{$art['art_thumbnail']}" alt="{$art['art_title']}">
-    <div>
-        <h4>{$art['art_title']}</h4>
-        <p>{$art['art_summary']}</p>
-    </div>
-</div>
-
-HTML;
-
-        endwhile;
+        // Loop para obter e exibir os artigos
+        while ($art = $res->fetch_assoc())
+            $search_view .= view_article($art['art_id']);
 
     // Se não achou nada:
     else :
-        $search_view .= <<<HTML
-
-<h2>Procurando por {$query}</h2>
-<p class="center">Nenhum conteúdo encontrado com "{$query}".</p>
-
-HTML;
-
+        $search_view .= "<p class=\"center\">Nenhum conteúdo encontrado com '{$query}'.</p>";
     endif;
 
 else :
@@ -123,14 +106,13 @@ require('_header.php');
     ?>
 </article>
 
-<aside> 
-    
-<?php 
+<aside>
+    <?php
     // Mostra os artigos mais visualizados
     require('widgets/_mostviewed.php');
-    $aside_viewSearch;
-    // Mostra os artigos mais comentados
-    require('widgets/mostcommented.php');
+    // Caso a busca retorne mais que 4 itens, exibe artigos mais comentados.
+    if ($total > 4)
+        require('widgets/_mostcommented.php');
     ?>
 </aside>
 
